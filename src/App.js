b8d8b8ba46d5efd444b295602d270718efcf8c05
vsreyes <virgilio.s.reyes@gmail.com>
2021-02-19 23:23:53 +0100

    Task setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
