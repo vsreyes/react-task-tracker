0a5c05c3c6e9fb88f74c2dad9d0021b5b96c4f95
vsreyes <virgilio.s.reyes@gmail.com>
2021-02-20 20:18:44 +0100

    Task delete setup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Tasks from './components/Tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  // Delete Task
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
