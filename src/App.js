d2a06be238e02edb72f1b69d04dac5dfe6eeaa2d
vsreyes <virgilio.s.reyes@gmail.com>
2021-02-21 15:50:55 +0100

    Server setup

--- src/App.js
+++ src/App.js
@@ -49,8 +49,11 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
